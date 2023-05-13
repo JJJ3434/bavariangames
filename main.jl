5f74441e0490ec1dce971d5cd330ae9cec68ce1f
Johannes <jjjojo34@gmail.com>
2023-05-13 21:58:27 +0200

    add Basic setup to work with excel sheet

--- main.jl
+++ main.jl
@@ -1,3 +1,13 @@
 module BG
-using GoogleSheets
+using XLSX
+using DataFrames
+
+Gruppen = DataFrame(XLSX.readtable("BavarianGames.xlsx", "Gruppe"))
+
+PktMasskrugschieben = DataFrame(XLSX.readtable("BavarianGames.xlsx", "PktMasskrugschieben"))
+PktWackelturm = DataFrame(XLSX.readtable("BavarianGames.xlsx", "PktWackelturm"))
+PktHolzscheitelwurf = DataFrame(XLSX.readtable("BavarianGames.xlsx", "PktHolzscheitelwurf"))
+PktReifenwuchten = DataFrame(XLSX.readtable("BavarianGames.xlsx", "PktReifenwuchten"))
+PktBulldogziehen = DataFrame(XLSX.readtable("BavarianGames.xlsx", "PktBulldogziehen"))
+
 end
