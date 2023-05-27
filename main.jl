a71b4598c1ae356fddb1b485b88f1448227960b8
Johannes Janker <63916210+JJJ3434@users.noreply.github.com>
2023-05-27 14:57:36 +0200

    add more fis points

--- main.jl
+++ main.jl
@@ -15,7 +15,7 @@ PktBierkruglauf = DataFrame(XLSX.readtable("BavarianGames.xlsx", "PktBierkruglau
 # wichtig:
 # teams in excel muessen in der richtigen reihenfolge nach id eingetragen werden
 #
-FISPkt = [100 80 60 50 45 40 36 32 29 26 24 22 20 18 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+FISPkt = [100 80 60 50 45 40 36 32 29 26 24 22 20 18 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
 
 function addLokalFIS!(Pkt::DataFrame, col::Symbol)
   # add lokal fis points
